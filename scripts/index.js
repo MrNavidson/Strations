92489b73099141de2e8d3d777cbfe58313592d53
MrNavidson <david-wyatt@utulsa.edu>
2016-12-27 15:12:02 -0600

    Two columns of 3
    What doesn't work:
    - New Word button

--- scripts/index.js
+++ scripts/index.js
@@ -88,14 +88,14 @@ function removeItem(array, index) {
 function createNewGame() {
 	var trs = [];
 	for (var i = 0; i < NUMBER_OF_WORDS; i++) {
-		if (!trs[i % 6]) {
-			trs[i % 6] = "";
+		if (!trs[i % 3]) {
+			trs[i % 3] = "";
 		}
 		var randomNumber = Math.floor(Math.random() * sessionData.length);
 		var word = sessionData[randomNumber];
 		removeItem(sessionData, randomNumber);
 		wordsSelected.push(word);
-		trs[i % 6] += "<div class=\"word\" id=\'" + i + "\' onclick=\"clicked(\'" + i + "\')\"><div><a href=\"#\"><span class=\"ada\"></span>" + word + "</a></div></div>";
+		trs[i % 3] += "<div class=\"word\" id=\'" + i + "\' onclick=\"clicked(\'" + i + "\')\"><div><a href=\"#\"><span class=\"ada\"></span>" + word + "</a></div></div>";
 	}
 	//<a href="#"><span class="ada">Washington stimulates economic growth </span>Read me</a>
 	for (var i = 0; i < trs.length; i++) {
